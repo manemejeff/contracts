c25b5e01697c14abeaf72ae3a7a263950dc9ebe0
manemejeff <kmb14056@yandex.ru>
2021-05-23 22:36:43 +0400

    models created added requirements.txt

--- main_app/contracts/admin.py
+++ main_app/contracts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contract, ContractType, Organization, Currency
 
 # Register your models here.
+
+admin.site.register(Contract)
+admin.site.register(ContractType)
+admin.site.register(Organization)
+admin.site.register(Currency)
--- main_app/contracts/models.py
+++ main_app/contracts/models.py
@@ -1,3 +1,44 @@
+from datetime import date
 from django.db import models
 
+
 # Create your models here.
+class Organization(models.Model):
+    organization_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.organization_name
+
+
+class ContractType(models.Model):
+    type_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.type_name
+
+class Currency(models.Model):
+    name = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Contract(models.Model):
+    organization = models.ForeignKey(to=Organization, on_delete=models.CASCADE)
+    type = models.ForeignKey(to=ContractType, on_delete=models.CASCADE)
+    currency = models.ForeignKey(to=Currency, on_delete=models.CASCADE)
+    contract_start_date = models.DateField(
+        auto_now=False,
+        auto_now_add=False,
+        default=date.today
+    )
+    contract_end_date = models.DateField(
+        auto_now=False,
+        auto_now_add=False,
+        default=date.today
+    )
+    contract_amount = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return f"Контракт {self.organization.organization_name} на сумму {self.contract_amount} {self.currency.symbol}"
