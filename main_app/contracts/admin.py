fbaa41f3746ec265dca899d5e852b6b9289bad93
manemejeff <kmb14056@yandex.ru>
2021-06-07 13:33:25 +0400

    started admin action

--- main_app/contracts/admin.py
+++ main_app/contracts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Contract, ContractType, Organization, Currency
+import random
 
 # Register your models here.
 
@@ -7,3 +8,15 @@ admin.site.register(Contract)
 admin.site.register(ContractType)
 admin.site.register(Organization)
 admin.site.register(Currency)
+
+
+# @admin.action(description='Create 10 contracts in db with random data')
+# def create_10_contracts_random():
+#     c = Contract(
+#         organization=random.choice(Organization.objects.all()),
+#         type=random.choice(ContractType.objects.all()),
+#         currency=random.choice(Currency.objects.all()),
+#         contract_start_date='',
+#         contract_end_date='',
+#         contract_amount=''
+#     )
